75c54b3383028b048e481bca980abf2bdd256081
Anjie Li <lenwhite@users.noreply.github.com>
2019-05-07 16:28:46 +0800

    Remove bootstrap

--- ui/src/PizzaCo/PizzaCo.js
+++ ui/src/PizzaCo/PizzaCo.js
@@ -1,32 +1,12 @@
 import React from 'react';
 
 import { Helmet } from 'react-helmet';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 
 const PizzaCo = (props) => {
   return <>
     <Helmet>
       <title>Pizza Co.</title>
     </Helmet>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Pizza Co.</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
   </>
 };
 
--- ui/src/index.js
+++ ui/src/index.js
@@ -16,12 +16,7 @@ class App extends Component {
     return (
       <Router>
         <Helmet>
-          <link
-            rel="stylesheet"
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-            crossorigin="anonymous"
-          />
+
         </Helmet>
         <Switch>
           <Route path="/pizza_co" component={PizzaCo} />
