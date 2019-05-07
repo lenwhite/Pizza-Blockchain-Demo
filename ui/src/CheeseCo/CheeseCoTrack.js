e0e7abc028ecca279ee507681def65ec942435aa
Anjie Li <lenwhite@users.noreply.github.com>
2019-05-07 16:24:48 +0800

    Remove bootstrap

--- ui/src/CheeseCo/CheeseCoTrack.js
+++ ui/src/CheeseCo/CheeseCoTrack.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import Popover from 'react-bootstrap/Popover';
-import Overlay from 'react-bootstrap/Overlay';
-import Button from 'react-bootstrap/Button';
-import Tooltip from 'react-bootstrap/Tooltip';
-import ListGroup from 'react-bootstrap/ListGroup';
+import { Container, Step } from 'semantic-ui-react';
 
 class CheeseCoTrack extends Component {
 
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return <div className="container mt-3">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    <hr />
-      <ProgressBar striped now={3/4 * 100} />
-      <p />
-      <ListGroup>
-        <ListGroup.Item variant="success">Prepared for shipping</ListGroup.Item>
-        <ListGroup.Item variant="success">In transport</ListGroup.Item>
-        <ListGroup.Item variant="success">Delivered</ListGroup.Item>
-        <ListGroup.Item>Paid</ListGroup.Item>
-      </ListGroup>
-    </div>
+    return <Container>
+      Lorem Ipsum
+      <hr />
+      <Step.Group ordered size='mini' widths={4}>
+        <Step completed>
+          <Step.Content>
+            <Step.Title>
+              Production
+            </Step.Title>
+            <Step.Description>
+              Produce cheese for PO
+            </Step.Description>
+          </Step.Content>
+        </Step>
+
+        <Step active>
+          <Step.Title>
+            Packing
+          </Step.Title>
+          <Step.Description>
+            Pack cheese for delivery
+          </Step.Description>
+        </Step>
+
+        <Step>
+          <Step.Title>
+            Shipping
+          </Step.Title>
+          <Step.Description>
+            Cheese transferred to carrier
+          </Step.Description>
+        </Step>
+
+        <Step>
+          <Step.Title>
+            Payment
+          </Step.Title>
+          <Step.Description>
+            Payment received for PO
+          </Step.Description>
+        </Step>
+      </Step.Group>
+
+    </Container>
   }
 }
 
