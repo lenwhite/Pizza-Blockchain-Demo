128e4cbf68ca957e965fe76a5868e4ce5215dfa4
Len <lenwhite@users.noreply.github.com>
2019-05-29 01:30:22 +0800

    Add basic auth

--- api/src/app.js
+++ api/src/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import tokenRouter from './routes/Token.route';
+import basicAuth from 'express-basic-auth';
 import bodyParser from 'body-parser';
+
+import CONFIG from './CONFIG';
 
 const app = express();
 const port = process.env.PORT || 6000;
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   // Request headers you wish to allow
@@ -19,10 +22,26 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(bodyParser.json({ type: 'application/json'})); 
-//app.use(bodyParser.json());                                     
-//app.use(bodyParser.urlencoded({extended: true}));               
-//app.use(bodyParser.text());     
+app.use(basicAuth({
+  // custom authorizer that only checks that userName matches any userName
+  // defined in config file
+  authorizer: (userName) => {
+    let isMatch = false;
+
+    Object.keys(CONFIG).forEach(entity => {
+      if (userName === CONFIG[entity].userName) {
+        isMatch = true;
+      }
+    });
+
+    return isMatch;
+  }
+}));
+
+app.use(bodyParser.json({ type: 'application/json' }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.text());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/Token', tokenRouter);
